aa463052c8c8fdc406593bf0ec660780d29bb815
ReQuetzal <nisvanhdzgcia@gmail.com>
2026-09-12 10:51:38 -0600

    BorrandoLoInutil

--- mi-app-frontend/vite.config.ts
+++ mi-app-frontend/vite.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,3 +6,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
+=======
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+>>>>>>> 0d46b38 (BorrandoLoInutil)
