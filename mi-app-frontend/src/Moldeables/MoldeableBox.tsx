bf7b5125fa3df0b2222444ff415cd353df37a3e8
ReQuetzal <nisvanhdzgcia@gmail.com>
2026-09-12 13:25:49 -0600

    MoldeableBox

--- mi-app-frontend/src/Moldeables/MoldeableBox.tsx
+++ mi-app-frontend/src/Moldeables/MoldeableBox.tsx
@@ -1,11 +1,5 @@
 import React, { useState, useRef, useCallback } from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import TrustBar from "./components/Trustbar";
-import Products from "./components/Products";
-import Security from "./components/Security";
-import CTAFooter from "./components/CTAfooter";
-import Footer from "./components/Footer";
+
 
 interface Size {
   width: number;
@@ -16,7 +10,7 @@ type Direction = "right" | "bottom" | "corner";
 
 
 
-const ResizableBox: React.FC = () => {
+export default function ResizableBox() {
   const [size, setSize] = useState<Size>({ width: 300, height: 200 });
   const containerRef = useRef<HTMLDivElement>(null);
   const startPos = useRef({ x: 0, y: 0, width: 0, height: 0 });
@@ -65,15 +59,7 @@ const ResizableBox: React.FC = () => {
   );
 
   return (
-    <><div className="min-h-screen bg-paper">
-      <Navbar />
-      <Hero />
-      <TrustBar />
-      <Products />
-      <Security />
-      <CTAFooter />
-      <Footer />
-    </div>
+
     
     <div
       ref={containerRef}
@@ -131,7 +117,6 @@ const ResizableBox: React.FC = () => {
       />
     </div>
 
-    </>
   );
 };
 
